76368d99f548ffcddc3885b9c55bdfe405348480
Jameel-Ahmed-arch <jameel41274@gmail.com>
2025-03-06 16:18:37 +0500

    Update Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -5,3 +5,21 @@ int main() {
     cout << "Your Name - Roll Number" << endl;
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+Division
+    if (b != 0) {
+        cout << "Quotient: " << a / b << endl;
+    } else {
+        cout << "Error! Division by zero." << endl;
+    }
+
+    cout << "Product: " << a * b << endl;
+Multiplication
+    return 0;
+}
